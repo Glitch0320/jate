8b6534fcb2fae13967f2d5d5901397d7091b663a
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-11-14 15:38:38 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Just Another Text Editor
+
+## Description
+This project was handed to me in the form of starter code given as part of the curriculum for the U of MN's coding bootcamp, as well as a repo provided by our instructor.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+
+## Usage
+
+
+## Screenshot
+<img src='' alt='' />
+
+## Credits
+
+### Collaborators
+
+
+### Third Party Attribution
+
+
+### Resources
+
+
+
+## License
+Liscense under the  liscense.
+
+## Features
+
+
+## How to Contribute
+
+
+## Tests
+
+
+## Questions
+Contact me by email: 
+
+---
---
+++ client/src/js/database.js
@@ -0,0 +1,55 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+/*
+  We need to add some code below which will take updated content and save it to IndexedDB.
+*/
+export const putDb = async (content) => {
+  // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
+  const db = await openDB('jate', 1);
+
+  // Now create a variable for the transaction
+  const tx = db.transaction('jate', 'readwrite');
+
+  // Now create a variable for the store
+  const store = tx.objectStore('jate');
+
+  // Now create a variable named "request" and have it perform the update
+  const request = store.put({ id: 1, value: content });
+
+  const result = await request;
+  console.log('🚀 - data saved to the database', result.value);
+};
+
+/*
+  We need to add some code below which will get all content from IndexedDB.
+*/
+export const getDb = async () => {
+  // You can duplicate the same first lines of code from above, except that the transaction will be 'readonly'
+  
+  const db = await openDB('jate', 1)
+
+  const tx = db.transaction('jate', 'readonly')
+
+  const store = tx.objectStore('jate')
+
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log('🚀 - data retrieved from the database', result.value)
+    : console.log('🚀 - data not found in the database');
+  return result?.value;
+};
+
+initdb();

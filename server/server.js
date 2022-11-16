dde720fc526381f1f4b86dab0aeeb74df364c3e2
Glitch0320 <100811040+Glitch0320@users.noreply.github.com>
2022-11-15 21:13:48 -0600

    Final

--- server/server.js
+++ server/server.js
@@ -2,11 +2,16 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+console.log(process.env)
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
